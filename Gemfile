9012b644df698508e227b7170751bf27e10288b7
JayTeeSF <jonathan@his-service.net>
2011-07-02 13:17:04 -0700

    updgrade to bundler -- specs broken (w/ Ruby 1.9 -- looking for test-unit)

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://gemcutter.org'
+# source "http://rubygems.org"
+gem 'rake', '0.8.7'
+gem 'will_paginate', '2.3.14'
+gem 'faker', '0.3.1'
+
+group :test do
+  gem "rspec", ">= 1.3.0"
+  gem "rspec-rails", ">= 1.3.2"
+  gem "factory_girl", ">= 1.2.4"
+end
--- config/initializers/fat_free_crm.rb
+++ config/initializers/fat_free_crm.rb
@@ -1,8 +1,10 @@
 require "fat_free_crm"
+require 'will_paginate'
 
 #---------------------------------------------------------------------
 Sass::Plugin.options[:template_location] = File.join(RAILS_ROOT, "app/stylesheets")
 Sass::Plugin.options[:css_location] = File.join(RAILS_ROOT, "public/stylesheets")
 
 #---------------------------------------------------------------------
+
 WillPaginate::ViewHelpers.pagination_options[:renderer] = "AjaxWillPaginate"
